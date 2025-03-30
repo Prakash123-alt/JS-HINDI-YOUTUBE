bff2f39136c988779645bb505fe1b815f5bb0544
Chejarla Prakash Rao <prakashraochejarla@gmail.com>
2025-03-30 18:23:14 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,57 @@
+"use strict"; // Treat all js code as a newer version
+
+// alert(3 + 3); // We are using nodejs not browser
+
+console.log(3 + 3); // Code readability should be high
+
+console.log("Prakash");
+
+let name = "Prakash"
+let age = 23
+let isLoggedIn = false
+let state;
+
+console.log(typeof isLoggedIn)
+
+/* PRIMITIVE DATA-TYEPS :
+
+   1.String:
+    -Represents textual data.
+    -Enclosed in single quotes (' '), double quotes (" "), or backticks ().
+    Example: "hello", 'world', JavaScript.
+
+   2.Number:
+    -Represents numeric values, including integers and floating-point numbers.
+    -JavaScript has only one number type.
+    -Also includes special values like Infinity, -Infinity, and NaN (Not-a-Number).
+    Example: 42, 3.14, -10.
+
+   3.Boolean:
+    -Represents logical values.
+    -Has only two possible values: true or false.
+    -Used for conditional logic.
+
+   4.Undefined:
+    -Represents a variable that has been declared but has not been assigned a value.
+    -A variable's default value is undefined.
+
+   5.Null:
+    -Represents the intentional absence of any object value.
+    -It's an assignment value, indicating that a variable has no value.
+
+   6.Symbol:
+    -Introduced in ECMAScript 2015 (ES6).
+    -Represents a unique and immutable value.
+    -Primarily used as object property keys to avoid naming conflicts.
+
+   7.BigInt:
+    -Introduced relatively recently.
+    -Represents integer values that are too large to be represented by the Number type.
+    -Allows you to safely store and operate on large integers. */
+
+// OBJECT :
+
+console.log(typeof undefined); // un-defined
+
+console.log(typeof null);      // object
+
