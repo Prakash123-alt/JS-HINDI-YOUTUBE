64e7d659591aedcd8e0c438fc5e8a4902d473e78
Chejarla Prakash Rao <prakashraochejarla@gmail.com>
2025-03-31 05:46:24 +0000

    data types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = 'prakashraochejarla@gmail.com'
+var accountPassword = 'Prakash@123'
+accountCity = 'Nellore'
+/* accountId = 512007 */
+
+let accountState = null;
+
+accountEmail    = 'prakashchejarla2002@gmail.com'
+accountPassword = 'Prakash@779'
+accountCity     = 'Hyderabad'
+
+console.log(accountId)
+console.log(typeof accountPassword)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
